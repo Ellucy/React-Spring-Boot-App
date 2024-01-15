12a292753663123777e0a441bf708efa6972ad95
Eleri Mets <eleri.apsolon@gmail.com>
2024-01-15 21:14:03 +0200

    Get All Employees REST API

--- src/main/java/net/javaguides/ems/service/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems/service/EmployeeServiceImpl.java
@@ -8,6 +8,9 @@ import net.javaguides.ems.mapper.EmployeeMapper;
 import net.javaguides.ems.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -26,9 +29,18 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public EmployeeDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId)
-                .orElseThrow(()->
+                .orElseThrow(() ->
                         new ResourceNotFoundException("Employee with given id (" + employeeId + ") does not exist"));
 
         return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+
+        return employees.stream()
+                .map(employee -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
     }
 }
