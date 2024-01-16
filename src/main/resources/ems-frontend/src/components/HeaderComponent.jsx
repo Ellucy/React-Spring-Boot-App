6c5595044e35a93e75d666f24c1cb7da33f5d7ab
Eleri Mets <eleri.apsolon@gmail.com>
2024-01-16 11:05:43 +0200

    Add 2 new components- HeaderComponent and FooterComponent. For header layout use bootstrap, for footer create custom css inside App.css. Add components to App.jsx.

---
+++ src/main/resources/ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-dark bg-dark">
+          <a
+            className="navbar-brand"
+            href="https://github.com/Ellucy/React-Spring-Boot-App"
+          >
+            Employee Management System
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
